85945cf091db29e361f90acc6e5f77cf0f499427
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-03-27 17:10:20 +0200

    Add files via upload

---
+++ ConsoleApp27/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Account
+    {
+        private static int NumberOfAcc;
+        private readonly int accountNumber;
+        private readonly Customer accountOwner;
+        private int maxMinusAllowed;
+        public int AccountNumber { get; }
+        public int Balance { get; private set; }
+        public Customer AccountOwner { get;}
+        public int MaxMinusAllowed { get;}
+
+        public Account(int accountNum, Customer c1, int MontlyIncome)
+        {
+            accountNumber++;
+            AccountNumber = accountNumber;
+            MaxMinusAllowed = MontlyIncome * 3;
+        }
+        public static bool operator ==(Account a1,Account a2)
+        {
+            if (a1 == a2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Account a1, Account a2)
+        {
+            if (a1 != a2)
+                return false;
+            else
+                return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Customer)
+            {
+                Account a = obj as Account;
+                return this == a;
+            }
+            return false;
+        }
+
+        public static bool operator +(Account a1,Account a2)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp27/Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Customer
+    {
+        private static int NumberOfCust;
+        private readonly int customerID;
+        private readonly int customerNumber;
+        public string Name { get; private set; }
+        public int PhNumber { get; private set; }
+        public int CustomerID { get; }
+        public int CustomerNumber { get; }
+
+        public Customer(string name, int phone, int id)
+        {
+            this.Name = name;
+            this.PhNumber = phone;
+            this.CustomerID = id;
+            customerNumber++;
+            CustomerNumber = customerNumber;
+
+        }
+
+        public static bool operator ==(Customer c1,Customer c2)
+        {
+            if (c1 == c2)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Customer c1,Customer c2)
+        {
+            if (c1 != c2)
+                return false;
+            else
+                return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Customer)
+            {
+                Customer c = obj as Customer;
+                return this == c;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.CustomerNumber;
+        }
+
+        public override string ToString()
+        {
+            return ($"Name:{Name},ID Number:{CustomerID},Customer Number:{CustomerNumber},Phone Number:{PhNumber}");
+        }
+    }
+}
