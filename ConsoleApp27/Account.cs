219daf4d4656524804708880b04fd23f51cd691c
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-03-27 19:32:20 +0200

    Update Account.cs

--- ConsoleApp27/Account.cs
+++ ConsoleApp27/Account.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +8,41 @@ namespace ConsoleApp27
 {
     class Account
     {
-        private static int NumberOfAcc;
+        private static int numberOfAcc = 0;
         private readonly int accountNumber;
         private readonly Customer accountOwner;
         private int maxMinusAllowed;
-        public int AccountNumber { get; }
-        public int Balance { get; private set; }
-        public Customer AccountOwner { get;}
-        public int MaxMinusAllowed { get;}
-
-        public Account(int accountNum, Customer c1, int MontlyIncome)
+        public int AccountNumber
         {
-            accountNumber++;
-            AccountNumber = accountNumber;
-            MaxMinusAllowed = MontlyIncome * 3;
+            get
+            {
+                return accountNumber;
+            }
+        }
+        public int Balance { get; private set; }
+        public Customer AccountOwner
+        {
+            get
+            {
+                return accountOwner;
+            }
+        }
+        public int MaxMinusAllowed
+        {
+            get
+            {
+                return maxMinusAllowed;
+            }
+
+        }
+
+        public Account(Customer customer, int montlyIncome)
+        {
+            this.accountOwner = customer;
+            this.maxMinusAllowed = montlyIncome * -3;
+            numberOfAcc++;
+            this.accountNumber = numberOfAcc;
+            this.Balance = 0;
         }
         public static bool operator ==(Account a1,Account a2)
         {
@@ -41,7 +62,7 @@ namespace ConsoleApp27
 
         public override bool Equals(object obj)
         {
-            if (obj is Customer)
+            if (obj is Account)
             {
                 Account a = obj as Account;
                 return this == a;
@@ -49,9 +70,27 @@ namespace ConsoleApp27
             return false;
         }
 
-        public static bool operator +(Account a1,Account a2)
+        public static Account operator +(Account a1, Account a2)
         {
+            int NewBalance = 0;
+            if (a1.accountOwner != a2.accountOwner)
+            {
+                throw new NotSameCustomerException();
+            }
+            else
+            {
+                if (a1.maxMinusAllowed < a2.maxMinusAllowed)
+                    NewBalance = a1.maxMinusAllowed / -3;
+                else
+                    NewBalance = a2.maxMinusAllowed /-3;
+                Account a3 = new Account(a1.accountOwner, NewBalance);
+                a3.Balance = a1.Balance + a2.Balance;              
+                a3.maxMinusAllowed = a1.maxMinusAllowed;
+                return a3;
+            }
+            
 
         }
+
     }
 }
